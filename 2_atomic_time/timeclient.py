4e7909236dcf43fb3f3ec34f3bc02b527a58e274
James Watson <jmjw1991@outlook.com>
2025-01-18 17:49:33 +0000

    Creating atomic time client

---
+++ 2_atomic_time/timeclient.py
@@ -0,0 +1,19 @@
+import logging
+import sockets
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class WebClient:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
