8210c54c6a3dcd74173d0c526ab9c3e6802e0c05
James Watson <jmjw1991@outlook.com>
2025-01-18 15:10:25 +0000

    Extension 2

--- 1_a_better_web_server/webserver.py
+++ 1_a_better_web_server/webserver.py
@@ -30,11 +30,49 @@ def process_arguments(system_arguments: sys.argv) -> int:
     return system_arguments[1]
 
 
-class WebServer:
+class ContentService:
+    """Web server was getting a bit too fat so moving all of the content generation to here"""
     def __init__(self, port: int, encoding: str = "ISO-8859-1") -> None:
         self.encoding = encoding
-        self.debug = False
-        self.files_path = Path("./files/") if not self.debug else Path("./1_a_better_web_server/files/")
+        self.resources_dir = "./resources/"
+        self.server_files = os.listdir(f"{self.resources_dir}/files/")
+        self.server_files_html = "".join([f'<li><a href="localhost:{port}/{i}">{i}</a></li>' for i in self.server_files])
+
+    def _read_file(self, file_name: str | Path, read_type: str = "rb") -> bytes:
+        with open(file_name, read_type) as f:
+            data = f.read()
+        return data
+
+    def _encode_msg(self, msg: str) -> bytes:
+        return msg.encode(self.encoding)
+    
+    def get_response(self, file_name: str | None = None) -> bytes:
+        if not file_name:
+            file_name = "home.html"
+            homepage = self._read_file(Path(f"{self.resources_dir}/{file_name}"), read_type="r")
+            homepage = homepage.replace("[SERVER_FILES]", self.server_files_html)
+            content = self._encode_msg(homepage)
+        else:
+            content = self._read_file(Path(f"{self.resources_dir}/files/{file_name}"))
+        
+        content_type = mimetypes.guess_type(file_name)[0]
+        response_partial = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}; charset={self.encoding.lower()}\r\nContent-Length: {len(content)}\r\nConnection: close\r\n\r\n"
+        response = self._encode_msg(response_partial) + content + b"\r\n"
+        return response
+
+    def not_implemented_response(self) -> bytes:
+        server_response = "HTTP/1.1 501 Not Implemented\r\n"
+        return self._encode_msg(server_response)
+    
+    def not_found_response(self) -> bytes:
+        server_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\n404 not found\r\n"
+        return self._encode_msg(server_response)
+
+
+class WebServer:
+    def __init__(self, port: int, content_service: ContentService, encoding: str = "ISO-8859-1") -> None:
+        self.encoding = encoding
+        self.content_service = content_service
 
         # Port initialization
         self.port = port
@@ -42,42 +80,11 @@ class WebServer:
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind(('', port))
         
-        # Server files
-        self.server_files = os.listdir(self.files_path)
+        # Pre-computed responses, should probably do it for all files but I'm lazy..
+        self.not_implemented = self.content_service.not_implemented_response()
+        self.not_found = self.content_service.not_found_response()
+        self.get = self.content_service.get_response()
 
-        # Pre-computed responses
-        self.not_implemented = self._not_implemented_response()
-        self.not_found = self._not_found_response()
-        self.get = self._get_response()
-
-    def _read_file(self, file_name: str) -> str:
-        with open(f"{self.files_path}/{file_name}", "rb") as f:
-            data = f.read()
-        return data
-
-    def _encode_msg(self, msg: str) -> bytes:
-        return msg.encode(self.encoding)
-
-    def _get_response(self, file_name: str | None = None) -> bytes:
-        if not file_name:
-            dir_options = f"Available files: {", ".join(self.server_files)}"
-            server_response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset={self.encoding.lower()}\r\nContent-Length: {len(dir_options)}\r\nConnection: close\r\n\r\n{dir_options}\r\n"
-            return self._encode_msg(server_response)
-        else:
-            content_type = mimetypes.guess_type(file_name)[0]
-            content = self._read_file(file_name)
-            server_response_pre = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}; charset={self.encoding.lower()}\r\nContent-Length: {len(content)}\r\nConnection: close\r\n\r\n"
-            server_response = self._encode_msg(server_response_pre) + content + b"\r\n"
-            return server_response
-    
-    def _not_implemented_response(self) -> bytes:
-        server_response = "HTTP/1.1 501 Not Implemented\r\n"
-        return self._encode_msg(server_response)
-    
-    def _not_found_response(self) -> bytes:
-        server_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\n404 not found\r\n"
-        return self._encode_msg(server_response)
-    
     def _parse_request(self, request: str) -> bytes:
         request_lines = request.split("\r\n")
         request_type, full_path, _ = request_lines[0].split(" ")
@@ -87,9 +94,9 @@ class WebServer:
         file_name = os.path.split(full_path)[-1]
         if full_path == "/" or file_name == "/":
             return self.get
-        if file_name not in self.server_files:
+        if file_name not in self.content_service.server_files:
             return self.not_found
-        return self._get_response(file_name)
+        return self.content_service.get_response(file_name)
         
     def listen(self) -> None:
         """
@@ -117,7 +124,8 @@ class WebServer:
 
 def main():
     port = process_arguments(sys.argv)
-    web_server = WebServer(port)
+    content_service = ContentService(port)
+    web_server = WebServer(port, content_service)
     web_server.listen()
 
 
